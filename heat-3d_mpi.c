4733a3d4feeb22159b40d2784f96da3405d1bf2f
Dantes4u <smile2000.ilya@yandex.ru>
2020-12-17 16:47:52 +0300

    Update heat-3d_mpi.c

--- heat-3d_mpi.c
+++ heat-3d_mpi.c
@@ -20,19 +20,19 @@ int main(int argc, char** argv)
 {
 	int n = N;
 	MPI_Request requests[4];
-    MPI_Status statuses[4];
+    	MPI_Status statuses[4];
 	
 	int myrank, ranksize;
-    int start_row, last_row, nrow;
-    double start, end, time;
+    	int start_row, last_row, nrow;
+    	double start, end, time;
 	
 	MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &ranksize);
-    MPI_Barrier(MPI_COMM_WORLD);
+    	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    	MPI_Comm_size(MPI_COMM_WORLD, &ranksize);
+    	MPI_Barrier(MPI_COMM_WORLD);
 	
 	start_row = (myrank * n) / ranksize;
-    last_row = (((myrank + 1) * n) / ranksize) - 1;
+    	last_row = (((myrank + 1) * n) / ranksize) - 1;
 	nrow = last_row - start_row + 1;
 	
 	for (i = start_row; i <= last_row; i++)
@@ -44,26 +44,26 @@ int main(int argc, char** argv)
 	for (t = 1; t <= TSTEPS; t++) 
 	{
 		if (myrank) {
-            MPI_Irecv(A[start_row - 1], n*n, MPI_DOUBLE, myrank - 1, 1215, MPI_COMM_WORLD, requests);
-        }
-        if (myrank != ranksize - 1) {
-            MPI_Isend(A[last_row], n*n, MPI_DOUBLE, myrank + 1, 1215, MPI_COMM_WORLD, requests + 2);
-        }
+		    MPI_Irecv(A[start_row - 1], n*n, MPI_DOUBLE, myrank - 1, 1215, MPI_COMM_WORLD, requests);
+		}
 		if (myrank != ranksize - 1) {
-            MPI_Irecv(A[last_row + 1], n * n, MPI_DOUBLE, myrank + 1, 1216, MPI_COMM_WORLD, requests + 3);
-        }
-        if (myrank) {
-            MPI_Isend(A[start_row], n * n, MPI_DOUBLE, myrank - 1, 1216, MPI_COMM_WORLD, requests + 1);
-        }
+		    MPI_Isend(A[last_row], n*n, MPI_DOUBLE, myrank + 1, 1215, MPI_COMM_WORLD, requests + 2);
+		}
+		if (myrank != ranksize - 1) {
+		    MPI_Irecv(A[last_row + 1], n * n, MPI_DOUBLE, myrank + 1, 1216, MPI_COMM_WORLD, requests + 3);
+		}
+		if (myrank) {
+		    MPI_Isend(A[start_row], n * n, MPI_DOUBLE, myrank - 1, 1216, MPI_COMM_WORLD, requests + 1);
+		}
 		int count = 4, shift = 0;
-        if (!myrank) {
-            count -= 2;
-            shift = 2;
-        }
-        if (myrank == ranksize - 1) {
-            count -= 2;
-        }
-        MPI_Waitall(count, requests + shift, statuses);
+		if (!myrank) {
+		    count -= 2;
+		    shift = 2;
+		}
+		if (myrank == ranksize - 1) {
+		    count -= 2;
+		}
+		MPI_Waitall(count, requests + shift, statuses);
 		for (i = start_row; i <= last_row; i++) {
 			if((!i) || (i == n-1)){
 				continue;
@@ -80,26 +80,26 @@ int main(int argc, char** argv)
 		
 		
 		if (myrank) {
-            MPI_Irecv(B[start_row - 1], n*n, MPI_DOUBLE, myrank - 1, 1215, MPI_COMM_WORLD, requests);
-        }
-        if (myrank != ranksize - 1) {
-            MPI_Isend(B[last_row], n*n, MPI_DOUBLE, myrank + 1, 1215, MPI_COMM_WORLD, requests + 2);
-        }
+		    MPI_Irecv(B[start_row - 1], n*n, MPI_DOUBLE, myrank - 1, 1215, MPI_COMM_WORLD, requests);
+		}
 		if (myrank != ranksize - 1) {
-            MPI_Irecv(B[last_row + 1], n * n, MPI_DOUBLE, myrank + 1, 1216, MPI_COMM_WORLD, requests + 3);
-        }
-        if (myrank) {
-            MPI_Isend(B[start_row], n * n, MPI_DOUBLE, myrank - 1, 1216, MPI_COMM_WORLD, requests + 1);
-        }
+		    MPI_Isend(B[last_row], n*n, MPI_DOUBLE, myrank + 1, 1215, MPI_COMM_WORLD, requests + 2);
+		}
+		if (myrank != ranksize - 1) {
+		    MPI_Irecv(B[last_row + 1], n * n, MPI_DOUBLE, myrank + 1, 1216, MPI_COMM_WORLD, requests + 3);
+		}
+		if (myrank) {
+		    MPI_Isend(B[start_row], n * n, MPI_DOUBLE, myrank - 1, 1216, MPI_COMM_WORLD, requests + 1);
+		}
 		count = 4, shift = 0;
-        if (!myrank) {
-            count -= 2;
-            shift = 2;
-        }
-        if (myrank == ranksize - 1) {
-            count -= 2;
-        }
-        MPI_Waitall(count, requests + shift, statuses);		
+		if (!myrank) {
+		    count -= 2;
+		    shift = 2;
+		}
+		if (myrank == ranksize - 1) {
+		    count -= 2;
+		}
+		MPI_Waitall(count, requests + shift, statuses);		
 		for (i = start_row; i <= last_row; i++) {
 			if((!i) || (i == n-1)){
 				continue;
